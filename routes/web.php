4286df635b22f3bc75b19403885a0ee84cad35f4
dilusha07 <dilushasandaruwani07@gmail.com>
2025-06-11 21:56:52 +0530

    Create api route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\StudentController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::apiResource('/student', StudentController::class);
